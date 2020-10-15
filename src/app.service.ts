073b8d25a670107c64b92b8e733d71e9f9d91f76
Gabriel Stamato <stamato7@gmail.com>
2020-10-15 17:59:21 -0300

    first commit

---
+++ src/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AppService {
+  googleLogin(req) {
+    if (!req.user) {
+      return 'No user from google';
+    }
+
+    return {
+      message: 'User information from google',
+      user: req.user,
+    };
+  }
+
+  async findOrCreate(profile) {
+    return console.log(profile);
+  }
+}
